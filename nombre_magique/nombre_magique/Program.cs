aa203d2a00c89f7af7ace4ebb903ca9e592a6aef
David Foudi <62246853+DFoudi@users.noreply.github.com>
2024-04-26 23:28:13 +0200

    Ajout du jeu "Nombre Magique"
    Ce commit ajoute le petit jeu du  "Nombre Magique" en C#.

---
+++ nombre_magique/nombre_magique/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace nombre_magique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Quele est le nombre magique ?");
+            Console.WriteLine("Veuillez choisir un nombre entre 1 et 100:");
+            Console.WriteLine();
+            try
+            {
+                // Nombre hasard :
+                Random rnd = new Random();
+                int nombreMagique = rnd.Next(1, 101);
+
+                int nombre = 0;
+
+                while (nombre != nombreMagique)
+                {
+                    // nombre choisi
+                    string nombreStr = Console.ReadLine();
+                    nombre = int.Parse(nombreStr);
+
+                    if (nombre > nombreMagique)
+                    {
+                        Console.WriteLine("Trop haut!");
+                    }
+                    else if (nombre < nombreMagique)
+                    {
+                        Console.WriteLine("Trop bas!");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bravo!");
+                        break;
+                    }
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+          
+        }
+    }
+}
